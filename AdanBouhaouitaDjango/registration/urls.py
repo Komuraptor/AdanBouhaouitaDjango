91277b29c3769b6fb5bf349b6957d08caa4eeabe
Adán bouhaouita <50923257+Komuraptor@users.noreply.github.com>
2022-05-11 13:09:21 +0200

    Commit

--- AdanBouhaouitaDjango/registration/urls.py
+++ AdanBouhaouitaDjango/registration/urls.py
@@ -1,13 +1,7 @@
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import include
-from django.conf import settings
-from django.conf.urls.static import static
+from .views import SignUpView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('', include('gestionOfertas.urls')),
-]+static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
-
-urlpatterns = static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    path("signup/", SignUpView.as_view(), name="signup"),
+]
